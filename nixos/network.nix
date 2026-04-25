f0dfe3bc5607107eeb6fb954dd162b04d8132bdb
macbucheron1 <nathandeprat@hotmail.fr>
2026-04-25 23:16:02 +0200

    feat: working bridge

--- nixos/network.nix
+++ nixos/network.nix
@@ -4,25 +4,23 @@ let
 in
 {
   networking = {
-    nameservers = [ "8.8.8.8" "1.1.1.1" ];
-
     hostName = lib.mkDefault hostname;
-
-    interfaces.eth0.useDHCP = lib.mkDefault true;
-  };
-  
-  systemd.services.dhcpcd-var-run = {
-    description = "Prepare /var/run for dhcpcd";
-    requiredBy = [ "dhcpcd.service" ];
-    before = [ "dhcpcd.service" ];
-    serviceConfig.Type = "oneshot";
-    script = ''
-      if [ ! -e /var/run ]; then
-        ln -s /run /var/run
-      fi
-      mkdir -p /run/dhcpcd
-    '';
+    useNetworkd = true;
+    useHostResolvConf = false;
+    nameservers = [ "8.8.8.8" "1.1.1.1" ];
   };
 
+  services.resolved.enable = false;
 
+  systemd.network.enable = true;
+  systemd.network.networks."10-eth0" = {
+    matchConfig.Name = "eth0";
+    address = [ "10.58.55.250/24" ];
+    routes = [
+      { Gateway = "10.58.55.1"; }
+    ];
+    networkConfig = {
+      IPv6AcceptRA = true;
+    };
+  };
 }
